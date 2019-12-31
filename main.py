b0508856e9f852d231ecc2f63934ec53dade9020
Masaki <masaki0114likebs@icloud.com>
2019-12-31 19:33:30 +0900

    変更してみたよ

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-# numの回数文ポーカーのペア判定を繰り返す
-import collections
-num = input()
-for x in range(int(num)):
-	answer = "No Pair"
-	line = list(str(input()))
-	c = collections.Counter(line)
-	if len(c) == 3:
-		answer = "One Pair"
-	elif len(c) == 2:
-		if c.most_common()[0][1]!=3:
-			answer = "Two Pair"
-		else:
-			answer = "Three Card"
-	elif len(c) == 1:
-		answer = "Four Card"
-	print(answer)
+input_line = int(input())
+for i in range(input_line):
+  s = input().rstrip().split(' ')
+  print("hello = "+s[0]+" , world = "+s[1])
